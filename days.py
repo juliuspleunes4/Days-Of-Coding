14f04fcea4f6411dc4ff3440848aab48386450ad
Julius Pleunes <38988786+juliuspleunes4@users.noreply.github.com>
2025-01-25 09:45:38 +0100

    Day 1

---
+++ days.py
@@ -0,0 +1,34 @@
+"""
+PYTHON: 365 DAYS OF CODING CHALLENGE
+Gedurende het jaar zal de moeilijkheid van beginner tot geavanceerd gaan!
+"""
+
+
+"""
+Day 1: Schrijf een Python-programma dat een getal van de gebruiker vraagt, en vervolgens het kwadraat van dat getal berekent en toont.
+"""
+def kwadraat_van_input():
+    getal = int(input("Geef een getal: "))
+    print(f"Het kwadraat van {getal} is {getal**2}")
+
+kwadraat_van_input()
+
+
+"""
+Day 2: Schrijf een Python-programma dat de gebruiker vraagt om een getal, en vervolgens toont of het getal even of oneven is.
+"""
+
+
+"""
+Day 3: Schrijf een Python-programma dat de gebruiker vraagt om een getal, en vervolgens toont of het getal positief, negatief of nul is.
+"""
+
+
+"""
+Day 4: Schrijf een Python-programma dat de gebruiker vraagt om een getal, en vervolgens toont of het getal een veelvoud is van 5.
+"""
+
+
+"""
+Day 5: Schrijf een Python-programma dat de gebruiker vraagt om een woord, en vervolgens dat woord omdraait en toont.
+"""
