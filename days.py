fe70b55dc44c1b6a9ed386c622fb40c9f2561704
Julius Pleunes <38988786+juliuspleunes4@users.noreply.github.com>
2025-01-28 10:17:06 +0100

    Day 2 & Day 3

--- days.py
+++ days.py
@@ -11,17 +11,39 @@ def kwadraat_van_input():
     getal = int(input("Geef een getal: "))
     print(f"Het kwadraat van {getal} is {getal**2}")
 
-kwadraat_van_input()
+# kwadraat_van_input()
 
 
 """
 Day 2: Schrijf een Python-programma dat de gebruiker vraagt om een getal, en vervolgens toont of het getal even of oneven is.
 """
+def getal_vragen():
+    user_input = input("Geef een getal: ")
+    user_input = int(user_input)
+    if user_input % 2 == 0:
+        print(f"{user_input} is een EVEN getal")
+    else:
+        print(f"{user_input} is een ONEVEN getal")
+
+# getal_vragen()
 
 
 """
 Day 3: Schrijf een Python-programma dat de gebruiker vraagt om een getal, en vervolgens toont of het getal positief, negatief of nul is.
 """
+def getal_waarde():
+    opvragen = input("Geef een getal: ")
+    opvragen = float(opvragen)
+    if opvragen == 0:
+        print(f"{opvragen} is gelijk aan 0!")
+    if opvragen > 0:
+        print(f"{opvragen} is een POSITIEF getal!")
+    if opvragen < 0:
+        print(f"{opvragen} is een NEGATIEF getal!")
+
+# getal_waarde()
+
+
 
 
 """
